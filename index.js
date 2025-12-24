d3dc3c7a3fced99dcc66d45c2cb986f3937146d4
Kingstar <ksbussiness3901@gmail.com>
2025-12-24 20:23:19 +0530

    first123456

--- index.js
+++ index.js
@@ -17,7 +17,13 @@ dotenv.config();
 const app = express();
 
 // Middlewares
-app.use(cors());
+app.use(
+  cors({
+    origin: ["*","http://localhost:5173"],
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true,
+  })
+);
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
